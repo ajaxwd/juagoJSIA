44cb0e31cff44d7e40d6767df402f30fabcafaf6
ajaxwd <ajaxwd@gmail.com>
2016-06-14 22:18:33 -0400

    separacion de clases base

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,11 +1,21 @@
 var board = new Array(3);
 
+function ClearCell(x, y) {
+  board[x][y] = 0;
+  cell = document.getElementById("c" + x + y);
+  cell.innerHTML = "";
+}
+
 funcion ClearBoard(){
   for (var i = 0; i < 3; i++)
     for (var j= 0; j < 3;j++){
-      board[i][j] = 0;
+      ClearCell(i, j)
     }
 
+}
+function PaintCell(x, y) {
+  cell = document.getElementById("c" + x + y);
+  cell.innerHTML = "<img src=ball.png></img>";
 }
 function autoplay(){
 
