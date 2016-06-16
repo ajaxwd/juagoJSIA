2383677620ba2e5f37bc0cdff44dc946bd2d2701
ajaxwd <ajaxwd@gmail.com>
2016-06-15 22:33:29 -0400

    mejora clase js principal

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,4 +1,5 @@
 var board = new Array(3);
+var turn;
 
 function ClearCell(x, y) {
   board[x][y] = 0;
@@ -15,13 +16,24 @@ funcion ClearBoard(){
 }
 function PaintCell(x, y) {
   cell = document.getElementById("c" + x + y);
-  cell.innerHTML = "<img src=ball.png></img>";
+  cell.innerHTML = "<img src=" + turn + ".png></img>";
+
+  if (turn = "ball") {
+    board[x][y] = 1;
+  } else {
+    board[x][y] = 2;
+  }
+
+  if (turn == "ball") turn = "cross";
+  else turn = "ball";
+
 }
 function autoplay(){
 
   for (var i = 0; i < 3; i++) board[i] = new Array(3);
-
+  turn = "ball";
   ClearBoard();
+  PaintCell(0,1);
 
 }
 
