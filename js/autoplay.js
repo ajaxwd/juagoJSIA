5352563044ba14f20911106ccf5321ba69f182fb
ajaxwd <ajaxwd@gmail.com>
2016-06-26 00:07:27 -0400

    se mejora la mecanica del juego

--- js/autoplay.js
+++ js/autoplay.js
@@ -25,7 +25,7 @@ function PaintCell(x, y) {
   cell = document.getElementById("c" + x + y);
   cell.innerHTML = "<img src=" + turn + ".png></img>";
 
-  if (turn = "ball") {
+  if (turn == "ball") {
     board[x][y] = 1;
     Ball_Sellected_x = x;
     Ball_Sellected_y = y;
@@ -39,9 +39,23 @@ function PaintCell(x, y) {
   else turn = "ball";
 
 }
+function CheckCell(x, y){
+  crosses = CheckTurn_Count(2);
+  if (crosses == 3) {
+    if (board[x][y] == 2) {
+      Cross_Sellected_x = x;
+      Cross_Sellected_y = y;
+      ClearCell(x, y);
+    }
+  }
+  else{
+    if (board[x][y] == 0 && DifMov(x, y)) SelectCell(x, y);
+  }
+}
 
 function SelectCell(x ,y) {
   PaintCell(x, y);
+  SearchMove();
 }
 
 function autoplay(){
--- js/smart.js
+++ js/smart.js
@@ -1,11 +1,28 @@
 
 function SearchMove() {
+  balls = CheckTurn_Count(1);
+  if (balls == 3) {
+    CellFind = false;
+
+    while (CellFind == false) {
+      x = Math.round(Math.random()*2);
+      y = Math.round(Math.random()*2);
+      if (board[x][y] == 1) CellFind = true;
+    }
+    Ball_Sellected_x = x;
+    Ball_Sellected_y = y;
+    ClearCell(x, y);
+  }
+  RandomMove();
+}
+
+function RandomMove(){
   CellAvaible = false;
 
   while (CellAvaible == false) {
     x = Math.round(Math.random()*2);
     y = Math.round(Math.random()*2);
-    if (board[x][y] == 0) {
+    if (board[x][y] == 0 && DifMov(x, y)) {
       CellAvaible = true;
     }
   }
